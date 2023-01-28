77fc36759a870d908aec27178e87778a31d31c11
dcha7225 <danielcha0131@gmail.com>
2023-01-28 18:08:47 -0500

    prettyui

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
 </script>
 
 <template>
+
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/logo.png"  />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -16,6 +17,7 @@
     <upload />
     <CameraComponent/>
   </main>
+ 
 </template>
 
 <style scoped>
